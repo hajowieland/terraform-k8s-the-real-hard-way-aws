6a1adfd9b718b0f3bbc8b83989e16544b5fdd11c
hajowieland <mail@wieland.tech>
2019-11-24 12:31:40 +0100

    fix conditional check

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_route_table_association" "private-rtassoc" {
 # AWS Key Pair to generate
 # => If you want to use an existing AWS Key Pair one, set value for TF var.aws_key_pair_name
 resource "aws_key_pair" "ssh" {
-  count      = var.aws_key_pair_name != null ? 1 : 0
+  count      = var.aws_key_pair_name == null ? 1 : 0
   key_name   = "${var.owner}-${var.project}"
   public_key = file(var.ssh_public_key_path)
 }
